3fd8d808431c4795ee3e6cbb9b5581e4be17a598
sshaikh7 <64500570+sshaikh7@users.noreply.github.com>
2022-01-17 11:15:38 +0530

    Updated java file

--- simple.java
+++ simple.java
@@ -2,6 +2,6 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! India"); 
+        System.out.println("Hello, World! India, Delhi,Mumbai"); 
     }
 }
